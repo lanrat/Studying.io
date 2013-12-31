ea2781180d663c5501fd2694f89eb03d5d0322bf
Ian Foster <mrlanrat@gmail.com>
2013-12-31 14:18:48 -0800

    JSON downloader works

--- src/com/vorsk/studying/io/Home.java
+++ src/com/vorsk/studying/io/Home.java
@@ -6,8 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 
+
 public class Home extends SherlockActivity implements OnClickListener {
 
+	//used to save state
+	private static boolean hintShown = false;
+	private static boolean answerShown = false;
+
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,33 +24,48 @@ public class Home extends SherlockActivity implements OnClickListener {
         findViewById(R.id.hintButton).setOnClickListener(this);
         findViewById(R.id.questionButton).setOnClickListener(this);
         findViewById(R.id.answerButton).setOnClickListener(this);
-
-       
+        
+        updateDisplay();
+        
+        DownloadTask dl = new DownloadTask(this);
+        dl.execute();
     }
 
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.hintButton:
-			findViewById(R.id.hintBox).setVisibility(View.VISIBLE);
+			hintShown = true;
 			break;
 		case R.id.questionButton:
-			findViewById(R.id.answerBox).setVisibility(View.GONE);
-			findViewById(R.id.questionButton).setVisibility(View.GONE);
-			findViewById(R.id.answerButton).setVisibility(View.VISIBLE);
+			answerShown = false;
 			break;
 		case R.id.answerButton:
-			findViewById(R.id.answerBox).setVisibility(View.VISIBLE);
-			findViewById(R.id.questionButton).setVisibility(View.VISIBLE);
-			findViewById(R.id.answerButton).setVisibility(View.GONE);
+			answerShown = true;
 			break;
 		default:
 			break;
 		}
+		updateDisplay();
 		
 	}
-
-    
+	private void updateDisplay() {
+		if (hintShown) {
+			findViewById(R.id.hintBox).setVisibility(View.VISIBLE);
+		}else {
+			findViewById(R.id.hintBox).setVisibility(View.GONE);
+		}
+		
+		if (answerShown) {
+			findViewById(R.id.answerBox).setVisibility(View.VISIBLE);
+			findViewById(R.id.questionButton).setVisibility(View.VISIBLE);
+			findViewById(R.id.answerButton).setVisibility(View.GONE);
+		} else {
+			findViewById(R.id.answerBox).setVisibility(View.GONE);
+			findViewById(R.id.questionButton).setVisibility(View.GONE);
+			findViewById(R.id.answerButton).setVisibility(View.VISIBLE);
+		}
+	}
 
     /* No menu for now
     @Override
